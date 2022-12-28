c6ede37976d5638b5789d6c633c2c1bc14a66014
Hatem Mohamed <hatem.mohamed@creatifyteam.com>
2022-12-28 04:02:36 +0200

    cancel my test api keys

--- Config/app.php
+++ Config/app.php
@@ -5,8 +5,8 @@ return [
     'debug' => true,
     'services'  => [
         "stripe" => [
-            "publishable_key" => "pk_test_51HH3fnKyafSZdlUodQyaBx3MCH5xkca43gjb57PCOX2gZZigUYl3IG6Zsjj9BgHABFl0oBfCr4Qfa2tMcCvo4sa500ZbTtnuPG",
-            "secret_key"      => "sk_test_51HH3fnKyafSZdlUoXD9vhOzSq5MIbSMsKcbAoSY9gJSiW6nXHDDq3HUAOPbx1Alv9kPX4729MXjYnrMSsBMM7VwP00gOp5yiiC"
+            "publishable_key" => "",
+            "secret_key"      => ""
         ]
     ]
 ];
